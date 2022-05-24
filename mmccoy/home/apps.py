83a63d027fce4a2efee93fdd770567e67ed287ea
Mark McCoy <mhmccoy@email.sc.edu>
2022-05-23 22:51:38 -0400

    Restarting project

--- mmccoy/home/apps.py
+++ mmccoy/home/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SiteHomeConfig(AppConfig):
+class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'site_home'
+    name = 'home'
